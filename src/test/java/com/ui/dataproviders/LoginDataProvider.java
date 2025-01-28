7ef7d0b2e935670d7d2365efff1e75a268493788
sonal <sonalperera88@Gmail.com>
2025-01-29 02:12:34 +1030

    change the screenshot file and test date file paths

--- src/test/java/com/ui/dataproviders/LoginDataProvider.java
+++ src/test/java/com/ui/dataproviders/LoginDataProvider.java
@@ -19,7 +19,8 @@ public class LoginDataProvider {
     @DataProvider(name = "LoginTestDataProvider")
     public Iterator<Object[]> loginDataProvider() throws FileNotFoundException {
         Gson gson = new Gson();
-        File testDataFile = new File(System.getProperty("user.dir") + "\\testData\\loginData.json");
+       // File testDataFile = new File(System.getProperty("user.dir") + "\\testData\\loginData.json");
+        File testDataFile = new File("./testData/loginData.json");
         FileReader fileReader = new FileReader(testDataFile);
         TestData data = gson.fromJson(fileReader, TestData.class); // deserialization
 
