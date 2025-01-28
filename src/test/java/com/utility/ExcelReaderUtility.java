9886e4a5c76d57e05b1970c9cccf7e378d5890a4
sonal <sonalperera88@Gmail.com>
2025-01-29 09:41:27 +1030

    change the test data file paths

--- src/test/java/com/utility/ExcelReaderUtility.java
+++ src/test/java/com/utility/ExcelReaderUtility.java
@@ -17,7 +17,7 @@ public class ExcelReaderUtility {
 
     public static Iterator<User> readExcelFile (String fileName) {
 
-        File xlsxFile = new File(System.getProperty("user.dir") + "//testData//" + fileName);
+        File xlsxFile = new File("./testData/" + fileName);
 
         XSSFWorkbook xssfWorkbook = null;
         List<User> userList = null;
